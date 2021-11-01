9a280020f93f687249828fbca13cb5a516574dfb
Tamal Saha <tamal@appscode.com>
2021-11-01 10:14:05 -0700

    added-all

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"go.uber.org/zap/zapcore"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	// Using zap's preset constructors is the simplest way to get a feel for the
+	// package, but they don't allow much customization.
+
+	zap.NewExample()
+	zap.NewDevelopment()
+
+	// NewConsoleEncoder
+
+	encoderCfg := zap.NewProductionConfig()
+	encoderCfg.Encoding = "console"
+	encoderCfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	//core := zapcore.NewCore(zapcore.NewConsoleEncoder(encoderCfg.EncoderConfig), os.Stdout, zap.DebugLevel)
+	//logger := zap.New(core)
+
+
+	//zap.NewProduction()
+	//zap.NewExample()
+	logger, _ := encoderCfg.Build() // zap.NewDevelopment() // or NewProduction, or NewDevelopment
+	defer logger.Sync()
+
+	const url = "http://example.com"
+
+	// In most circumstances, use the SugaredLogger. It's 4-10x faster than most
+	// other structured logging packages and has a familiar, loosely-typed API.
+	sugar := logger.Sugar()
+	sugar.Infow("Failed to fetch URL.",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s", url)
+
+	// In the unusual situations where every microsecond matters, use the
+	// Logger. It's even faster than the SugaredLogger, but only supports
+	// structured logging.
+	logger.Info("Failed to fetch URL.",
+		// Structured context as strongly typed fields.
+		zap.String("url", url),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+}
